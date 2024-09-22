1a8d1493846d244fed9fca748c39c6579aa048f4
alotofnekos <107799901+alotofnekos@users.noreply.github.com>
2024-09-22 17:14:15 +0800

    can now open screens

--- CSOPESY.cpp
+++ CSOPESY.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <conio.h>
 #include <stdio.h>
+#include <ctime>
+#include <string>   
+#include <sstream>
 
 void banner(){
 	std::cout <<"\n";
@@ -27,6 +30,23 @@ void non_blocking()
 	std::cout << "q pressed, exiting non-blocking mode... "<< std::endl; 
 }
 
+
+
+std::string timestamp() {
+    time_t timestamp;
+    time(&timestamp);
+    struct tm *localTime = localtime(&timestamp);
+    char formattedTime[100];
+    strftime(formattedTime, sizeof(formattedTime), "%m/%d/%Y, %I:%M:%S %p", localTime);
+    return std::string(formattedTime);  
+}
+
+void screen(const std::string& screenName) {
+    std::string command = "start cmd /k \"echo Process name " + screenName + " opened at: " + timestamp() +
+                          " && echo Type 'exit' to close this screen.";
+    system(command.c_str());
+}
+
 int interpreter(std::string command){
 	int isExit = 0;
     if (command == "clear") {
@@ -34,9 +54,19 @@ int interpreter(std::string command){
         banner();
     } else if (command == "initialize") {
         std::cout << command << " command recognized. Doing something." << std::endl;
-    } else if (command == "screen") {
-        std::cout << command << " command recognized. Doing something." << std::endl;
-    } else if (command == "scheduler-test") {
+    } else if (command.rfind("screen -s", 0) == 0) {
+        std::istringstream iss(command);
+        std::string cmd, dash_s, screenName;
+        iss >> cmd >> dash_s >> screenName;
+        if (!screenName.empty()) {
+            std::cout << "Opening " << screenName << std::endl;
+            screen(screenName); 
+        } else {
+            std::cout << "Error: No screen name provided!" << std::endl;
+    	}
+    } else if (command.rfind("screen", 0) == 0) {
+    	std::cout << "Syntax: screen -s <name>" << std::endl;
+	} else if (command == "scheduler-test") {
         std::cout << command << " command recognized. Doing something." << std::endl;
     } else if (command == "scheduler-stop") {
         std::cout << command << " command recognized. Doing something." << std::endl;
